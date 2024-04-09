d41705bf694d07c88a7ca7b24bcab652375fed43
Bikash Adhikari <virtual.bikash@gmail.com>
2024-04-10 05:24:07 +0545

    notes in set added

--- set.py
+++ set.py
@@ -39,6 +39,29 @@ print(C)
 D = A.intersection(B) #---------------Set Intersection
 print(D)
 
+#=========================================================================
+'''-------SOME IMPORTANT NOTES ON SET-------'''
+#=========================================================================
+
+# Set is the collection of unordered items.
+# Each element in the set must be unique and immutable
+# We can store the following value in set
+    #---integer
+    #---float
+    #---string
+    #---tuple
+    #---boolean
+
+# BUT= List and Dictionaries can not be used since they are "MUTABLE"    
+# set has stored its value only inside the {} curly braces.
+# Duplicate values are ignored by set. If we store same value, set will execute only one value every time
+# print(lent(set))=== will print only the count of distinct value.
+
+# set is mutable [since it can be changed]
+# but elements in set are immutable [since they cant be changed]
+
+# in set union and intersection == it method returns the new set and dont change the old one
+
 
 
 
